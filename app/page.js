d8da575058b000fd0b9b6ddad7bd57ce202a86c0
saqlainmuhammadd <miansaqlainqureshi@gmail.com>
2024-08-05 17:12:25 +0100

    updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 'use client';
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { Firestore } from "firebase/firestore";
+import {firestore} from "@/firebase";
+import { collection, getDocs, query } from "firebase/firestore";
 import { Box, Typography } from "@mui/material";
 import { Fascinate } from "next/font/google";
 
@@ -9,6 +10,11 @@ export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState([false])
   const [itemName, setItemName] = useState([])
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+  }
   return <Box>
     <Typography variant="h2">
       Inventory Management
